4f030469a6359dfdf9a6183c4c45dda13429fecf
wolf <ahmedragabyasin2020@gmail.com>
2021-11-14 18:18:15 +0200

    front is  okkkkkk

--- resources/views/site/layouts/footerjs.blade.php
+++ resources/views/site/layouts/footerjs.blade.php
@@ -1,3 +1,25 @@
-<script src="site/assets/js/vendor.min.js?v=1557447064956"></script>
-<script src="site/assets/js/scripts.min.js?v=1557447064956"></script>
-<script src="site/assets/js/custom.js?v=1557447064956"></script>
+<script src="{{ asset('front/js/jquery-3.6.0.min') }}"></script>
+<script src="{{ asset('front/js/vendor.min.js?v=1557446391092') }}"></script>
+<script src="{{ asset('front/js/scripts.min.js?v=1557446391092') }}"></script>
+<script src="{{ asset('front/js/custom.js?v=1557446391092') }}"></script>
+<script src="{{ asset('front/js/owl.carousel.min') }}"></script>
+@yield('js')
+<script>
+    $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+</script>
